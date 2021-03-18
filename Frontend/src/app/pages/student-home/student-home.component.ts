6a5c67d85451c111b2ad9bf425dbe59e4189eab7
BryanJCuellar <bryanb194@yahoo.com>
2021-03-18 02:15:51 -0600

    Actualización aggregate & estilos

--- Frontend/src/app/pages/student-home/student-home.component.ts
+++ Frontend/src/app/pages/student-home/student-home.component.ts
@@ -12,9 +12,9 @@ export class StudentHomeComponent implements OnInit {
   backendHost: string = 'http://localhost:8888/';
   // backendHost: string = 'https://ingsoftware-backend.herokuapp.com/';
   estudianteActual: any;
-  ofertas: any;
+  ofertas = [];
   closeResult = '';
-  elegir='home';
+  elegir = 'home';
   color1 = "#520547";
   color2 = "#520547";
   color3 = "#520547";
@@ -24,7 +24,7 @@ export class StudentHomeComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private estudiantesService: EstudiantesService,
-    private OfertasService : OfertasService
+    private OfertasService: OfertasService
   ) { }
 
   ngOnInit(): void {
@@ -48,64 +48,72 @@ export class StudentHomeComponent implements OnInit {
     }
   }
 
-  obtenerOfertas(){
-   // console.log('Error al obtener informacion estudiante', error)
-   this.OfertasService.obtenerOfertas()
-   .subscribe(
-   res => {   
-     console.log("Ofertas", res);
-     // console.log(res[0].empresa[0].imagenPerfil);
-     this.ofertas = res;
-   },
-   error => console.log('Error al obterner ofertas', error)
-   )
+  obtenerOfertas() {
+    // console.log('Error al obtener informacion estudiante', error)
+    this.OfertasService.obtenerOfertas()
+      .subscribe(
+        res => {
+          // console.log("Ofertas", res);
+          // console.log(res[0].empresa[0].imagenPerfil);
+          for (let i = 0; i < res.length; i++) {
+            this.ofertas.push(res[i]);
+            if (res[i].descripcion.length > 50) {
+              this.ofertas[i].resumenDescripcion = (res[i].descripcion).substring(0, 49) + '...';
+            } else {
+              this.ofertas[i].resumenDescripcion = res[i].descripcion;
+            }
+          }
+          console.log("Ofertas", this.ofertas);
+        },
+        error => console.log('Error al obtener ofertas', error)
+      )
   }
 
   getAuthService() {
     return this.authService;
   }
-  
-  home(){
+
+  home() {
     this.color2 = "#520547";
     this.color3 = "#520547";
     this.color4 = "#520547";
     this.color5 = "#520547";
-    this.elegir='home';
+    this.elegir = 'home';
     this.color1 = '#854A7C';
   }
-  Mis_Postulaciones(){
+  Mis_Postulaciones() {
     this.color1 = "#520547";
     this.color3 = "#520547";
     this.color4 = "#520547";
     this.color5 = "#520547";
-    this.elegir='Mis_Postulaciones';
+    this.elegir = 'Mis_Postulaciones';
     this.color2 = '#854A7C';
   }
-  Buscar_Ofertas(){
+  Buscar_Ofertas() {
     this.color1 = "#520547";
     this.color2 = "#520547";
     this.color4 = "#520547";
     this.color5 = "#520547";
-    this.elegir='Buscar_Ofertas';
+    this.elegir = 'Buscar_Ofertas';
     this.color3 = '#854A7C';
   }
 
-  
-  Mi_Curriculum(){
+
+  Mi_Curriculum() {
     this.color1 = "#520547";
     this.color2 = "#520547";
     this.color3 = "#520547";
     this.color5 = "#520547";
-    this.elegir='Mi_Curriculum';
+    this.elegir = 'Mi_Curriculum';
     this.color4 = '#854A7C';
   }
 
-  Configuracion(){
+  Configuracion() {
     this.color1 = "#520547";
     this.color2 = "#520547";
     this.color3 = "#520547";
     this.color4 = "#520547";
-    this.elegir='Configuracion';
+    this.elegir = 'Configuracion';
     this.color5 = '#854A7C';
   }
 
